851d0f5ae0b0f1e114285e32c87c426ed2119a6b
NJ - 09DL 706051 - <120409444+NasvinJ@users.noreply.github.com>
2023-01-09 17:36:29 +0000

    tombs

--- main.py
+++ main.py
@@ -1,13 +1,12 @@
 #Imported all other files to run for the user to use in sequence
-import login_screen as log_scr
+import login_screen
 import loading_screen
 import home_page
 import game1_speed_typing_test
 import game2_internet_speed_test
-import game3_color_speed_reactiontest
 
 #Runs the login screen, then takes you to the home screen for the user to pick the test options
-# log_scr.login_screen_gui()
+login_screen.login_screen_gui()
 # loading_screen.loading_screen_gui()
 home_page.home_screen_gui()
 # game1_speed_typing_test.speed_typing_gui_screen()
